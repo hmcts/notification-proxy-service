17895750e3b12f7a74f6953e1b13807c2ec7cca2
Anooj Kurup <anooj.kurup@hmcts.net>
2022-04-27 13:04:44 +0100

    suppress cves

--- src/smokeTest/java/uk/gov/hmcts/reform/notifications/smoketests/SmokeTestConfiguration.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/notifications/smoketests/SmokeTestConfiguration.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.notifications.smoketests;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan("uk.gov.hmcts.reform.demo.smoketests")
+@ComponentScan("uk.gov.hmcts.reform.notifications.smoketests")
 @PropertySource("application.properties")
 public class SmokeTestConfiguration {
   //TODO: implement smoke tests here
