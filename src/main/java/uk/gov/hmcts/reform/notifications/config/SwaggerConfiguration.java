c8678e04462fb09e8598e5f9cc5970f6ce2aa728
“PragneshPatel2” <pragnesh.patel2@hmcts.net>
2022-12-20 00:32:43 +0000

    PAY-5851 change authorization false

--- src/main/java/uk/gov/hmcts/reform/notifications/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/notifications/config/SwaggerConfiguration.java
@@ -31,7 +31,7 @@ This needs to be uncommented when auth tokens are required to access the endpoin
             new ParameterBuilder()
                 .name("Authorization")
                 .description("User authorization header")
-                .required(true)
+                .required(false)
                 .parameterType("header")
                 .modelRef(new ModelRef("string"))
                 .build(),
