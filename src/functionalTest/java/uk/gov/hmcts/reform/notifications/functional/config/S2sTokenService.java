af48698fe39960df503f4ad41d0bcb23dc5af56d
kaushikmaitra <kaushik.maitra@hmcts.net>
2022-12-29 18:44:23 +0000

    removed log statements

--- src/functionalTest/java/uk/gov/hmcts/reform/notifications/functional/config/S2sTokenService.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/notifications/functional/config/S2sTokenService.java
@@ -11,9 +11,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class S2sTokenService {
 
-    private final TestConfigProperties testProps;
-
-
 
     private final OneTimePasswordFactory oneTimePasswordFactory;
     private final S2sApi s2sApi;
@@ -22,7 +19,6 @@ public class S2sTokenService {
     @Autowired
     public S2sTokenService(OneTimePasswordFactory oneTimePasswordFactory, TestConfigProperties testProps) {
         this.oneTimePasswordFactory = oneTimePasswordFactory;
-        this.testProps = testProps;
         s2sApi = Feign.builder()
             .encoder(new JacksonEncoder())
             .target(S2sApi.class, testProps.getS2sBaseUrl());
@@ -30,21 +26,6 @@ public class S2sTokenService {
 
     public String getS2sToken(String microservice, String secret) {
         String otp = oneTimePasswordFactory.validOneTimePassword(secret);
-        LOG.info("s2sApi : " + s2sApi.toString());
-        LOG.info("testProps : " + testProps.getS2sBaseUrl());
-        LOG.info("microservice : " + microservice);
-        LOG.info("secret : " + secret);
-        LOG.info("otp : " + otp);
-        String s2sToken = "";
-        try {
-            s2sToken = s2sApi.serviceToken(microservice, otp);
-            LOG.info("s2sToken : " + s2sToken);
-            s2sToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY3BheV9idWJibGUiLCJleHAiOjE2NzIzNTE2ODh9.b2l4DVZymh3X4pqXBQsBiFWr-qWPU9vmkv1HrrVg_8dsPIB4EWjKiqKQBo4yGohP0huK7UYhKXgsRKSyGIP0ig";
-            return s2sToken;
-        } catch (Exception ex) {
-            LOG.error("EXCEPTION in S2sTokenService Notifications-Service !!!");
-            LOG.info(ex.getMessage());
-        }
-        return null;
+        return s2sApi.serviceToken(microservice, otp);
     }
 }
