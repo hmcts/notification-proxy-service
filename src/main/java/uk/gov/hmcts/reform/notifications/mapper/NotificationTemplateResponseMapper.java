2bb3e24bdf68504c37806f76ebbf4fb59471fc29
Kumar <364915@192.168.1.238>
2023-01-17 17:38:43 +0000

    pmd fix

--- src/main/java/uk/gov/hmcts/reform/notifications/mapper/NotificationTemplateResponseMapper.java
+++ src/main/java/uk/gov/hmcts/reform/notifications/mapper/NotificationTemplateResponseMapper.java
@@ -80,7 +80,7 @@ public class NotificationTemplateResponseMapper {
 
         return FromTemplateContact.buildFromTemplateContactWith()
             .fromEmailAddress(toFromEmailMapper(notificationType, serviceContact))
-            .fromMailAddress(!(serviceContact.getFromMailAddress() == null)?toFromMailMapper(notificationType, serviceContact):null)
+            .fromMailAddress(toFromMailMapper(notificationType, serviceContact))
             .build();
     }
 
@@ -96,7 +96,7 @@ public class NotificationTemplateResponseMapper {
     private MailAddress toFromMailMapper(String notificationType, ServiceContact serviceContact) {
 
         MailAddress fromMailAddress = null;
-        if(LETTER.equalsIgnoreCase(notificationType)) {
+        if(null != serviceContact.getFromMailAddress() && LETTER.equalsIgnoreCase(notificationType)){
 
             fromMailAddress = MailAddress.buildRecipientMailAddressWith()
                 .addressLine(serviceContact.getFromMailAddress().getAddressLine())
