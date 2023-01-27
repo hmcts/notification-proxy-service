4a8c3106260c39d43cc0befa0dce8847bccd93eb
Kumar <364915@192.168.1.238>
2023-01-27 15:45:49 +0000

    header value response

--- src/main/java/uk/gov/hmcts/reform/notifications/dtos/response/HeaderInfo.java
+++ src/main/java/uk/gov/hmcts/reform/notifications/dtos/response/HeaderInfo.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Header {
+public class HeaderInfo {
     @JsonProperty("header")
     private HeaderDetails header;
 }
